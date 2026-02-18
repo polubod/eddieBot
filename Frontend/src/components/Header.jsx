6808343f5f6628373f7022787ceca613edfab8cf
Ryan Bender <152329584+bendery13@users.noreply.github.com>
2026-02-18 11:44:25 -0600

    Added banner image and favicon

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <div className="header">
-      <img src="/vite.svg" alt="EddieBot" className="logo" />
+      <img src="/SIUE_Logo.png" alt="SIUE logo" className="logo" />
       <h1 className="header-title">EddieBot</h1>
     </div>
   );
